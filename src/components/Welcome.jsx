a4bbd04f518fed19433a4f0be4271ff2fb27a0a0
alizaeri <90989935+alizaeri@users.noreply.github.com>
2022-04-18 22:44:30 +0200

    3

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,16 +10,17 @@ const Welcome =() => {
           <div>
               
               <div className="parent">
-                  <img src="pic.jpg" alt="" />
-                  <div className="text">
-                  <h1>Welcome to Doggy Daycare</h1>
-                  <h5>Press Button To Start</h5>
-                  <Link to="/List" >
-                  <button>Start
-                  </button>
-              </Link>
+                  
+                <img src="pic.jpg" alt=""/>
+                <div className="text">
+                    <h1>Welcome to Doggy Daycare</h1>
+                    <h5>Press Button To Start</h5>
+                    <Link to="/List" >
+                    <button>Start
+                    </button>
+                    </Link>
                       
-                  </div>
+                </div>
                
               </div>
         
